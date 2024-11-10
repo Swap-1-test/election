e0cddefd748a274b46403e07f01590f2466511f2
Swap-1-test <swap12321232@gmail.com>
2024-11-10 13:41:08 +0530

    updated code

--- result/app.py
+++ result/app.py
@@ -78,4 +78,4 @@ def download_voters():
         return f"Error: {e}", 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=3001)
--- vote/app.py
+++ vote/app.py
@@ -98,4 +98,4 @@ def thank_you():
     return render_template('thank_you.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=3000)
